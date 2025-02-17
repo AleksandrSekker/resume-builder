f4492dfb5df9d688d5422932c0e6c9c76fe8eec7
Oleksandr Sekker <oleksandr.sekker@gmail.com>
2025-02-17 10:52:20 +0200

    fix: fix problem for deploy

--- src/components/PDFWrapper/PDFWrapper.tsx
+++ src/components/PDFWrapper/PDFWrapper.tsx
@@ -1,13 +1,11 @@
-import dynamic from "next/dynamic";
-import {ResumePDF} from "@/components";
-import {FormValues} from "@/types";
-
-const PDFViewer = dynamic(() => import('@react-pdf/renderer').then(mod => mod.PDFViewer), { ssr: false });
+import { ResumePDF } from '@/components';
+import { FormValues } from '@/types';
+import { PDFViewer } from '@react-pdf/renderer';
 
 export const PDFWrapper = (children: FormValues) => {
-    return (
-        <PDFViewer width={'100%'} height={'95%'}>
-            <ResumePDF {...children} />
-        </PDFViewer>
-    );
+  return (
+    <PDFViewer width={'100%'} height={'95%'}>
+      <ResumePDF {...children} />
+    </PDFViewer>
+  );
 };
